515dd8507410d9e962412a00c054bdc168c20a5c
Ferdi DEMİRCİ <ferdidemirci@Ferdis-MacBook-Pro.local>
2023-12-19 10:22:06 +0300

    Initial commit.

---
+++ Games/GamesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Games
+//
+//  Created by Ferdi DEMİRCİ on 19.12.2023.
+//
+
+import UIKit
+
+class GamesViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+}
+
